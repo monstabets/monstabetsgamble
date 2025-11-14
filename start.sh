999c491c445bededda9a70298ce1a43023f20050
monstabets <hunterjmartyn@gmail.com>
2025-11-14 07:58:18 -0600

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,2 +1,8 @@
 #!/bin/bash
+
+# Start Flask keep-alive web server on Render's assigned port
+gunicorn monsta_sports_bot:app --bind 0.0.0.0:$PORT &
+
+# Start the betting bot loop
 python3 monsta_sports_bot.py
+
